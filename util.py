a1f20bde9f8d1961e4656b9f8520fe618a7b209c
Tim Diller <timdiller@gmail.com>
2023-10-02 15:20:29 -0500

    clean up

--- util.py
+++ util.py
@@ -34,8 +34,6 @@ def initialize_logging(console_level=logging.DEBUG,
     return logger
 
 
-
-
 def log_args(logger, level=logging.DEBUG, is_method=False):
     """Decorator to log arguments passed to func."""
     def inner_func(func):
@@ -43,21 +41,20 @@ def log_args(logger, level=logging.DEBUG, is_method=False):
         @wraps(func)
         def return_func(*args, **kwargs):
             arg_log_fmt = "{name}({arg_str})"
-            if is_method:
+            arg_list = []
+            if is_method:  # get class name from arg[0]
                 class_name = args[0].__class__.__name__
                 arg_log_fmt = class_name + "." + arg_log_fmt
                 if len(args) > 1:
-                    arg_list = list("{!r}".format(arg) for arg in args[1:])
-                else:
-                    arg_list = []
+                    arg_list.extend("{!r}".format(arg) for arg in args[1:])
             else:
-                arg_list = list("{!r}".format(arg) for arg in args)
+                arg_list.extend("{!r}".format(arg) for arg in args)
             arg_list.extend("{}={!r}".format(key, val)
                             for key, val in kwargs.items())
             msg = arg_log_fmt.format(name=func.__name__,
                                      arg_str=", ".join(arg_list))
             # set stacklevel=2 to get lineno from the file defining
-            # the function being wrapped, not this file.
+            # the function being wrapped, not this file.  Needs Python >= 3.8
             logger.log(level, msg, stacklevel=2)
             return func(*args, **kwargs)
         return return_func
@@ -65,22 +62,19 @@ def log_args(logger, level=logging.DEBUG, is_method=False):
 
 
 if __name__ == "__main__":
-    logger = initialize_logging()
+    initialize_logging()
+    logger = logging.getLogger(__name__)
 
     @log_args(logger)
     def foo(x, y, z):
         pass
 
     class Bar(object):
-        def __repr__(self):
-            return self.__class__.__name__ + "()"
         @log_args(logger, is_method=True)
         def baz(self, *args, **kwargs):
             pass
 
     foo(1, 2, z=3)
-    foo(1, 2, 3)
-    foo(x=1, y=2, z=3)
 
     bar = Bar()
     bar.baz(1, c=3, b=2)
